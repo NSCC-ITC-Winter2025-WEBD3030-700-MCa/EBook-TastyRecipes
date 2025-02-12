f58c08e7710577e4b626a2ca77cee98371d321e3
Hiren Gajjar <144141125+HGajjar17@users.noreply.github.com>
2025-02-12 09:37:09 -0400

    fix: updated the 404 page

--- tasty-ebooks/src/pages/404.js
+++ tasty-ebooks/src/pages/404.js
@@ -1,15 +1,28 @@
 import * as React from "react"
-
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="p-8 text-center">
+      <h1 className="text-6xl font-bold mb-4">Oops! Page Not Found</h1>
+      <StaticImage
+        src="../images/404-error-page.png"
+        alt="404 Page Not Found"
+        placeholder="blurred"
+        className="w-96 mx-auto"
+      />
+      <p className="text-xl mb-6">
+        The page you're looking for doesn't exist. It might have been removed, or the URL could be incorrect.
+      </p>
+      <a href="/" className="text-blue-500 underline">
+        Go back to the homepage
+      </a>
+    </div>
   </Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />
+export const Head = () => <Seo title="404: Page Not Found" />
 
 export default NotFoundPage
