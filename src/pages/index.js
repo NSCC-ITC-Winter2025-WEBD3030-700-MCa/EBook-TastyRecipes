8c4ee6ecf53ff2be81ab0c35e136ad8ccb4ef4e8
Kortney <kortney_foley@hotmail.com>
2025-02-21 14:56:17 -0400

    feat: add images to cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ const IndexPage = () => {
   // Sample recipes data with categories
   const recipes = [
     { id: 1, title: 'Delicious Spaghetti', imageurl: "/images/spaghetti.jpg", category: 'Pasta', description: 'Indulge in foreign flavors', link: '/recipes/spaghetti' },
-    { id: 2, title: 'Vegetable Stir-Fry', imageurl: "", category: 'Vegetarian', description: 'Healthy and delicious', link: '/recipes/vegetarian-stir-fry' },
-    { id: 3, title: 'Chocolate Cake', imageurl: "", category: 'Desserts', description: 'Sweet and rich dessert', link: '/recipes/chocolate-cake' },
-    { id: 4, title: 'Classic Lasagna', imageurl: "", category: 'Pasta', description: 'Layered with cheese and sauce', link: '/recipes/lasagna' },
+    { id: 2, title: 'Vegetable Stir-Fry', imageurl: "/images/vegStirFry.jpg", category: 'Vegetarian', description: 'Healthy and delicious', link: '/recipes/vegetarian-stir-fry' },
+    { id: 3, title: 'Chocolate Cake', imageurl: "/images/chocolatecake.jpg", category: 'Desserts', description: 'Sweet and rich dessert', link: '/recipes/chocolate-cake' },
+    { id: 4, title: 'Classic Lasagna', imageurl: "/images/lasagna.jpg", category: 'Pasta', description: 'Layered with cheese and sauce', link: '/recipes/lasagna' },
     { id: 4, title: "Breanna's Loaded Mashed Potatoes", imageurl: "/images/potatoes.jpg", category: 'Sides', description: 'Loaded with cheese and bacon bits', link: '/recipes/potatoes' },
   ];
 
